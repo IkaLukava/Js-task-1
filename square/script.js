e732d95cb37a40a1a05e6dada656cd3dd157c9de
Ika Lukava <iraklilukava06@gmail.com>
2023-11-30 15:50:33 +0400

    delete places

--- square/script.js
+++ square/script.js
@@ -4,10 +4,7 @@ let square1 = document.querySelector(".square");
 let main = document.getElementsByTagName("main")[0];
 let arvici = document.querySelector(".atrakeb")
 
-
-
 square1.addEventListener("click", function(){
-
 
     let randomWidth = `${Math.floor(Math.random() * (arvici.offsetWidth - 100))}px`;
     let randomHeight = `${Math.floor(Math.random() * (arvici.offsetHeight - 100))}px`;
