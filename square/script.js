205758fe4b8f4a27db48a33f31f466ce8e1aa50b
Ika Lukava <iraklilukava06@gmail.com>
2023-11-30 16:16:05 +0400

    update header and body style, update classes

--- square/script.js
+++ square/script.js
@@ -1,13 +1,12 @@
 console.log("random movement of the square");
 
-let square1 = document.querySelector(".square");
-let main = document.getElementsByTagName("main")[0];
-let arvici = document.querySelector(".atrakeb")
+const square = document.querySelector(".square");
+const container = document.querySelector(".container")
 
-square1.addEventListener("click", function(){
+square.addEventListener("click", function(){
+    const randomWidth = `${Math.floor(Math.random() * (container.offsetWidth - 100))}px`;
+    const randomHeight = `${Math.floor(Math.random() * (container.offsetHeight - 100))}px`;
 
-    let randomWidth = `${Math.floor(Math.random() * (arvici.offsetWidth - 100))}px`;
-    let randomHeight = `${Math.floor(Math.random() * (arvici.offsetHeight - 100))}px`;
-    square1.style.left = `${randomWidth}`;
-    square1.style.top = `${randomHeight}`;
+    square.style.left = `${randomWidth}`;
+    square.style.top = `${randomHeight}`;
 })
